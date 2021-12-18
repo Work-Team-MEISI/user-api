975ab51a8a043034e4ea2a34747bc06e8a88b470
ryzenboi98 <joaopro23@gmail.com>
2021-12-18 11:08:48 +0000

    Added the register of user functionality

--- main.py
+++ main.py
@@ -3,6 +3,29 @@ from schemas import User
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-   return {"Hello world!"}
+users = []
+
+@app.post("/users/create")
+async def create(user: User):
+    check_user = [u for u in users if u.username == user.username]
+
+    if check_user:
+        raise HTTPException(status_code=400, detail="Username already exists!")
+
+    if users:
+        user.id = users[-1].id + 1
+    else:
+        user.id = 1 
+    
+    users.append(user)
+
+    return {**{"id": user.id},**{"message": "Created with success!"}}
+
+@app.get("/users/{id}")
+async def detail(id):
+    user = [u for u in users if str(u.id) == id]
+
+    if user:
+        return user[0]
+    else:
+        raise HTTPException(status_code=404, detail=f"User with id {id} doesn't exist!")
